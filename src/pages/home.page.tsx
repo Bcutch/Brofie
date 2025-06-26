c9c0e93af4cc5a8f7a983e65560f829e9f9c8efa
Bcutch <bcutch13@gmail.com>
2025-06-25 23:38:25 -0400

    init

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,19 @@
+
+
+export const Home: React.FC = () => {
+
+    return (
+        <>
+            <title>
+                Hello World!
+            </title>
+            <body>
+                <div>
+                    <h1 className="text-center text-2xl">
+                        Testing
+                    </h1>
+                </div>
+            </body>
+        </>
+    )
+}
